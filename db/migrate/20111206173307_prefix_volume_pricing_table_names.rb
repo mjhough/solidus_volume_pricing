a489fce09d2cdad6bb90ccd5360c9ebbc40fec2c
Matt Hough <matt@hough.tv>
2018-09-17 23:27:29 +1000

    Patch to work with new setup

--- db/migrate/20111206173307_prefix_volume_pricing_table_names.rb
+++ db/migrate/20111206173307_prefix_volume_pricing_table_names.rb
@@ -1,5 +1,5 @@
 class PrefixVolumePricingTableNames < SolidusSupport::Migration[4.2]
   def change
-    rename_table :volume_prices, :spree_volume_prices unless Spree::VolumePrice.table_exists?
+    rename_table :volume_prices, :spree_volume_prices
   end
 end
